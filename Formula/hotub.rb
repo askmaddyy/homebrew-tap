f09e370286806447e15e63407dd9b22adfdb5dd1
Madhav Oberoi <askmaddyy@users.noreply.github.com>
2026-09-07 13:41:55 +0530

    git-pattern formula for private repo

--- Formula/hotub.rb
+++ Formula/hotub.rb
@@ -1,8 +1,9 @@
 class Hotub < Formula
   desc "Fire + water screensavers for your terminal"
   homepage "https://github.com/askmaddyy/hotub"
-  url "https://api.github.com/repos/askmaddyy/hotub/tarball/v0.1.0"
-  sha256 "fda4810699aff8610b3653d06e71edccb0b4b7ad23a88b7c0182f23f9dcfd5d4"
+  url "https://github.com/askmaddyy/hotub.git",
+      tag:      "v0.1.0",
+      revision: "3d8329f9816659d5e6930bd93c958c17d54d0f9a"
   depends_on "rust" => :build
 
   def install
