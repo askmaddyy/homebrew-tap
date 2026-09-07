aa0fcf56858530d9d2bab5f2a9fe949985815dbb
Madhav Oberoi <askmaddyy@users.noreply.github.com>
2026-09-07 13:40:45 +0530

    Use API tarball URL so token auth works

--- Formula/hotub.rb
+++ Formula/hotub.rb
@@ -1,7 +1,7 @@
 class Hotub < Formula
   desc "Fire + water screensavers for your terminal"
   homepage "https://github.com/askmaddyy/hotub"
-  url "https://github.com/askmaddyy/hotub/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://api.github.com/repos/askmaddyy/hotub/tarball/v0.1.0"
   sha256 "fda4810699aff8610b3653d06e71edccb0b4b7ad23a88b7c0182f23f9dcfd5d4"
   depends_on "rust" => :build
 
